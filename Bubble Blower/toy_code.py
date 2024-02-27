df000f0697ddf824d50462f5c24e58d88e1a950c
Anna Erpenbeck <acer227@uky.edu>
2024-02-27 16:56:06 -0500

    Updated code for bubble blower.

--- Bubble Blower/toy_code.py
+++ Bubble Blower/toy_code.py
@@ -3,101 +3,16 @@ import machine
 import pins
 
 def run_toy():
-    print("Running toy code")
-    
-    input_pin = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
-    
-    # Count how many times the button has been pressed in a one second interval 
-    button_press_count = 0
+ print("Running toy code")
 
-    # Boolean is the button currently being pressed
-    button_is_pressed = 0
+ input_pin = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
+ output_pin = machine.Pin(0, machine.Pin.OUT)  # Use pin0 for output
 
-    # Take the current time
-    timer_start = time.time()
+ while True:
+   if input_pin.value() == 0:  # Button is pressed
+     output_pin.high()  # Turn on pin0
+     time.sleep(4)  # Wait for 4 seconds
+     output_pin.low()  # Turn off pin0
+     time.sleep(0.5)  # Add a slight delay to prevent multiple triggers
 
-    # Loop forever
-    while True:
-        if (input_pin.value() == 0) and (button_is_pressed == 0):
-            print("Press " + str(time.time()))
-            print(button_press_count)
-            time.sleep(0.1)
-            #print("Button was pressed!")
-            button_press_count = button_press_count + 1
-            button_is_pressed = 1
-        elif(input_pin.value() != 0):
-            button_is_pressed = 0
-            
-        # Every second interpret any button presses
-        if(time.time() - timer_start >= 1):
-            if(button_press_count > 0):
-                play_vibration(button_press_count, input_pin)
-            button_press_count = 0
-            timer_start = time.time()
-
-# Play vibration pattern until button is released 
-def play_vibration(vibration_selection, input_pin):
-    output_pin = machine.Pin(20, machine.Pin.OUT)
-    while(input_pin.value() == 0):
-        print(vibration_selection)
-        if(vibration_selection == 1):
-            print("Play vibration 1")
-            output_pin.high()
-            time.sleep(4)
-            output_pin.low()
-        elif(vibration_selection == 2):
-            print("Play vibration 2")
-            output_pin.high()
-            time.sleep(1)
-            output_pin.low()
-            time.sleep(1)
-            output_pin.high()
-            time.sleep(1)
-            output_pin.low()
-        elif(vibration_selection >= 3):
-            print("Play vibration 3")
-            output_pin.high()
-            time.sleep(0.2)
-            output_pin.low()
-            time.sleep(0.2)
-            output_pin.high()
-            time.sleep(0.2)
-            output_pin.low()
-            time.sleep(0.2)
-            output_pin.high()
-            time.sleep(0.2)
-            output_pin.low()
-            time.sleep(0.2)
-            output_pin.high()
-            time.sleep(0.2)
-            output_pin.low()
-            time.sleep(0.2)
-            output_pin.high()
-            time.sleep(0.2)
-            output_pin.low()
-            time.sleep(0.2)
-            output_pin.high()
-            time.sleep(0.2)
-            output_pin.low()
-            time.sleep(0.2)
-            output_pin.high()
-            time.sleep(0.2)
-            output_pin.low()
-            time.sleep(0.2)
-            output_pin.high()
-            time.sleep(0.2)
-            output_pin.low()
-            time.sleep(0.2)
-            output_pin.high()
-            time.sleep(0.2)
-            output_pin.low()
-            time.sleep(0.2)
-            output_pin.high()
-            time.sleep(0.2)
-            output_pin.low()
-            time.sleep(0.2)
-        time.sleep(0.1)
-
-
-
-
+run_toy()
